0c6c8b2faf9beb00dc35e4a960268c8b463cf584
tututututututuuu <125288121+tututututututuuu@users.noreply.github.com>
2023-02-19 16:14:21 +0800

    Add files via upload

---
+++ 课设(1) - 副本/MedicalSystem/MedicalSystem/Desktop.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MedicalSystem
+{
+    public partial class Desktop : Form
+    {
+        public Desktop()
+        {
+            InitializeComponent();
+        }
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    register register = new register();
+        //    register.Show();
+        //    this.Close();
+        //}
+
+        //private void btnregisteration_Click(object sender, EventArgs e)
+        //{
+        //    registration registration = new registration();
+        //    registration.Show();
+        //    this.Close();
+        //}
+
+        //private void btnDoctorSee_Click(object sender, EventArgs e)
+        //{
+        //    FillMedicalRecord fillMedicalRecord = new FillMedicalRecord();
+        //    fillMedicalRecord.Show();
+        //    this.Close();
+        //}
+    }
+}
---
+++ 课设(1) - 副本/MedicalSystem/MedicalSystem/frm_Connection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Configuration;                                                                                                     //包含访问配置文件所需的配置管理器；需事先在本项目的“引用”中添加对System.Configuration的引用；
+using System.Data.SqlClient;                                                                                                    //包含访问SQL Server所需的各类对象；
+using System.Windows.Forms;
+
+namespace MedicalSystem
+{
+    public partial class frm_Connection : Form
+    {
+        public frm_Connection()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;                                                                //本窗体启动位置设为屏幕中央；
+            if (ConfigurationManager.ConnectionStrings["Sql"] != null)                                                          //若配置管理器从配置文件读取到指定的连接字符串；
+            {
+                SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();                      //声明并实例化SQL连接字符串构造器；
+                sqlConnectionStringBuilder.ConnectionString = ConfigurationManager.ConnectionStrings["Sql"].ConnectionString;   //借助配置管理器从配置文件读取完整的连接字符串，并赋予SQL连接字符串构造器的相应属性；
+                this.txb_Server.Text = sqlConnectionStringBuilder.DataSource;                                                   //从SQL连接字符串构造器各属性中，获取连接字符串的各个元素，并显示于相应控件上；
+                this.txb_Database.Text = sqlConnectionStringBuilder.InitialCatalog;
+                this.ckb_IsWindowsAuthentication.Checked = sqlConnectionStringBuilder.IntegratedSecurity;
+            }
+        }
+
+        private void btn_Connect_Click(object sender, EventArgs e)
+        {
+            SqlConnection sqlConnection = new SqlConnection();                                                                  //声明并实例化SQL连接；
+            sqlConnection.ConnectionString =
+                ConfigurationManager.ConnectionStrings["Sql"].ConnectionString;                                                 //配置管理器从配置文件读取连接字符串，并将之赋予SQL连接的连接字符串属性；
+            sqlConnection.Open();                                                                                               //打开SQL连接；
+            MessageBox.Show                                                                                                     //在消息框中显示；
+                ($"连接状态：{sqlConnection.State}" +                                                                            //消息框消息内容；
+                 $"\n工作站标识：{sqlConnection.WorkstationId}" +
+                 $"\n服务器地址：{sqlConnection.DataSource}" +
+                 $"\n服务器版本：{sqlConnection.ServerVersion}" +
+                 $"\n数据库名称：{sqlConnection.Database}" +
+                 $"\n\n（单击【确定】后将关闭SQL连接）");
+            sqlConnection.Close();
+        }
+
+        private void gpb_ConnectionParameter_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
